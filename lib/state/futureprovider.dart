704113ed02221d4ee0b660fb094cc46932b9d51e
bhuphathi <bhuphathi@gmail.com>
2023-09-03 21:14:17 +0530

    providers other usable methods

--- lib/state/futureprovider.dart
+++ lib/state/futureprovider.dart
@@ -6,3 +6,17 @@ final fetchFutureProvider = FutureProvider((ref) {
   final url = Uri.parse("http://jsonplaceholder.typicode.com/users/1");
   return http.get(url).then((value) => User.fromJson(value.body));
 });
+
+// final fetchFutureProvider = FutureProvider.autoDispose((ref) {
+//   final url = Uri.parse("http://jsonplaceholder.typicode.com/users/1");
+//   return http.get(url).then((value) => User.fromJson(value.body));
+// });
+
+// final fetchFutureProvider = FutureProvider.autoDispose((ref) {
+//   ref.keepAlive();
+//   ref.onDispose(() {});
+//   ref.onCancel(() {});
+//   ref.onResume(() {});
+//   final url = Uri.parse("http://jsonplaceholder.typicode.com/users/1");
+//   return http.get(url).then((value) => User.fromJson(value.body));
+// });
