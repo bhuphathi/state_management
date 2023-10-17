cdf20540fbd66dcc273cdf1ec4cf7cfe935745ba
bhuphathi <bhuphathi@gmail.com>
2023-10-17 12:21:55 +0530

    feat: enum added to state notifier

--- lib/state/user_statenotifier.dart
+++ lib/state/user_statenotifier.dart
@@ -1,57 +1,73 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'dart:convert';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+enum Role {
+  anonymous,
+  admin,
+  owner,
+  manager,
+  supervisor,
+}
+
 class User {
   final String name;
   final int age;
+  final Role role;
   User({
     required this.name,
     required this.age,
+    required this.role,
   });
 
   User copyWith({
     String? name,
     int? age,
+    Role? role,
   }) {
     return User(
       name: name ?? this.name,
       age: age ?? this.age,
+      role: role ?? this.role,
     );
   }
 
   Map<String, dynamic> toMap() {
-    final result = <String, dynamic>{};
-
-    result.addAll({'name': name});
-    result.addAll({'age': age});
-
-    return result;
+    return <String, dynamic>{
+      'name': name,
+      'age': age,
+      'role': role.toString(),
+    };
   }
 
   factory User.fromMap(Map<String, dynamic> map) {
     return User(
-      name: map['name'] ?? '',
-      age: map['age']?.toInt() ?? 0,
+      name: map['name'] as String,
+      age: map['age'] as int,
+      role: Role.values.firstWhere((e) => e.toString() == map["role"])
     );
   }
 
   String toJson() => json.encode(toMap());
 
-  factory User.fromJson(String source) => User.fromMap(json.decode(source));
+  factory User.fromJson(String source) => User.fromMap(json.decode(source) as Map<String, dynamic>);
 
   @override
-  String toString() => 'User(name: $name, age: $age)';
+  String toString() => 'User(name: $name, age: $age, role: $role)';
 
   @override
-  bool operator ==(Object other) {
+  bool operator ==(covariant User other) {
     if (identical(this, other)) return true;
-
-    return other is User && other.name == name && other.age == age;
+  
+    return 
+      other.name == name &&
+      other.age == age &&
+      other.role == role;
   }
 
   @override
-  int get hashCode => name.hashCode ^ age.hashCode;
+  int get hashCode => name.hashCode ^ age.hashCode ^ role.hashCode;
 }
 
 class UserNotifier extends StateNotifier<User> {
@@ -64,8 +80,12 @@ class UserNotifier extends StateNotifier<User> {
   void updateAge(int value) {
     state = state.copyWith(age: value);
   }
+
+  void updateRole(Role value) {
+    state = state.copyWith(role: value);
+  }
 }
 
 final userNotifierProvider = StateNotifierProvider<UserNotifier, User>((ref) {
-  return UserNotifier(User(name: "", age: 0));
+  return UserNotifier(User(name: "", age: 0, role: Role.anonymous));
 });
--- lib/widget/statenotifier_provider.dart
+++ lib/widget/statenotifier_provider.dart
@@ -7,10 +7,14 @@ class StateNotifierProviderWidget extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    Role selectedRole = Role.anonymous;
     final state = ref.watch(userNotifierProvider);
     // watch state for selected property or field in a state provider
-    final userNameSelect =
-        ref.watch(userNotifierProvider.select((value) => value.name));
+    final userNameSelect = ref.watch(userNotifierProvider.select((value) => value.name));
+    final userRole = ref.watch(userNotifierProvider.select((value) => value.role));
+    print(Role.admin);
+    print(Role.admin.toString());
+    print(userNameSelect);
 
     return Center(
       child: Padding(
@@ -21,8 +25,7 @@ class StateNotifierProviderWidget extends ConsumerWidget {
             Consumer(
               builder: (context, ref, child) {
                 final name = ref.read(userNotifierProvider);
-                return Text(
-                    "from Consumer provider widget: ${name.name} ${name.age}");
+                return Text("from Consumer provider widget: ${name.name} ${name.age}");
               },
             ),
             const SizedBox(height: 10),
@@ -56,9 +59,22 @@ class StateNotifierProviderWidget extends ConsumerWidget {
                 labelText: 'Age',
               ),
               onChanged: (value) {
-                ref
-                    .read(userNotifierProvider.notifier)
-                    .updateAge(int.parse(value));
+                ref.read(userNotifierProvider.notifier).updateAge(int.parse(value));
+              },
+            ),
+            const SizedBox(height: 10),
+            DropdownButtonFormField<Role>(
+              value: userRole,
+              decoration: const InputDecoration(border: OutlineInputBorder()),
+              items: Role.values
+                  .map((role) => DropdownMenuItem<Role>(
+                        value: role,
+                        child: Text(role.toString().split('.').last),
+                      ))
+                  .toList(),
+              onChanged: (newValue) {
+                print("role newValue: $newValue");
+                ref.read(userNotifierProvider.notifier).updateRole(newValue!);
               },
             ),
           ],
